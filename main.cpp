f1901ba7b623a61b1a1e65e9fb94029e812a8f5f
NewWorld188 <turingdev@163.com>
2021-11-27 21:51:06 +0800

    202111/17

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include<iostream>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    int a;
+    cout<<"this is a tesing"<<endl;
 
     return a.exec();
 }
